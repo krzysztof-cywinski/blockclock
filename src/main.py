0f1566a2d76fdd0ade5714f6226d58dfc0b3dd6f
Krzysztof Cywiński <k.m.cywinski@gmail.com>
2024-11-16 22:17:54 +0100

    Replaced the font

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ fontdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
 if os.path.exists(libdir):
     sys.path.append(libdir)
-    
+
 from TP_lib import gt1151
 from TP_lib import epd2in13_V4
 import time
@@ -51,14 +51,14 @@ def Read_BMP(File, x, y):
 
 try:
     logging.info("epd2in13_V4 Touch Demo")
-    
+
     epd = epd2in13_V4.EPD()
     gt = gt1151.GT1151()
     GT_Dev = gt1151.GT_Development()
     GT_Old = gt1151.GT_Development()
-    
+
     logging.info("init and Clear")
-    
+
     epd.init(epd.FULL_UPDATE)
     gt.GT_Init()
     epd.Clear(0xFF)
@@ -68,14 +68,14 @@ try:
     t.start()
 
     # Drawing on the image
-    font15 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 15)
-    font24 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 24)
-    
+    font15 = ImageFont.truetype(os.path.join(fontdir, 'bitcoin-webfont.ttf'), 15)
+    font24 = ImageFont.truetype(os.path.join(fontdir, 'bitcoin-webfont.ttf'), 24)
+
     image = Image.open(os.path.join(picdir, 'Menu.bmp'))
     epd.displayPartBaseImage(epd.getbuffer(image))
     DrawImage = ImageDraw.Draw(image)
     epd.init(epd.PART_UPDATE)
-    
+
     i = j = k = ReFlag = SelfFlag = Page = Photo_L = Photo_S = 0
     PhotoPath_S = [ "Photo_1_0.bmp",
                     "Photo_1_1.bmp", "Photo_1_2.bmp", "Photo_1_3.bmp", "Photo_1_4.bmp",
@@ -86,7 +86,7 @@ try:
                     "Photo_2_5.bmp", "Photo_2_6.bmp",
                     ]
     PagePath = ["Menu.bmp", "White_board.bmp", "Photo_1.bmp", "Photo_2.bmp"]
-    
+
     while(1):
         if(i > 12 or ReFlag == 1):
             if(Page == 1 and SelfFlag == 0):
@@ -117,7 +117,7 @@ try:
         gt.GT_Scan(GT_Dev, GT_Old)
         if(GT_Old.X[0] == GT_Dev.X[0] and GT_Old.Y[0] == GT_Dev.Y[0] and GT_Old.S[0] == GT_Dev.S[0]):
             continue
-        
+
         if(GT_Dev.TouchpointFlag):
             i += 1
             GT_Dev.TouchpointFlag = 0
@@ -129,51 +129,51 @@ try:
                     Read_BMP(PagePath[Page], 0, 0)
                     Show_Photo_Small(image, Photo_S)
                     ReFlag = 1
-                elif(GT_Dev.X[0] > 29 and GT_Dev.X[0] < 92 and GT_Dev.Y[0] > 153 and GT_Dev.Y[0] < 193): 
+                elif(GT_Dev.X[0] > 29 and GT_Dev.X[0] < 92 and GT_Dev.Y[0] > 153 and GT_Dev.Y[0] < 193):
                     print("Draw ...\r\n")
                     Page = 1
                     Read_BMP(PagePath[Page], 0, 0)
                     ReFlag = 1
-                
-            
+
+
             if(Page == 1 and ReFlag == 0):   #white board
                 DrawImage.rectangle([(GT_Dev.X[0], GT_Dev.Y[0]), (GT_Dev.X[0] + GT_Dev.S[0]/8 + 1, GT_Dev.Y[0] + GT_Dev.S[0]/8 + 1)], fill=0)
-                if(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 118 and GT_Dev.Y[0] > 6 and GT_Dev.Y[0] < 30): 
+                if(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 118 and GT_Dev.Y[0] > 6 and GT_Dev.Y[0] < 30):
                     print("Home ...\r\n")
                     Page = 1
                     Read_BMP(PagePath[Page], 0, 0)
                     ReFlag = 1
-                elif(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 118 and GT_Dev.Y[0] > 113 and GT_Dev.Y[0] < 136): 
+                elif(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 118 and GT_Dev.Y[0] > 113 and GT_Dev.Y[0] < 136):
                     print("Clear ...\r\n")
                     Page = 0
                     Read_BMP(PagePath[Page], 0, 0)
                     ReFlag = 1
-                elif(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 118 and GT_Dev.Y[0] > 220 and GT_Dev.Y[0] < 242): 
+                elif(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 118 and GT_Dev.Y[0] > 220 and GT_Dev.Y[0] < 242):
                     print("Refresh ...\r\n")
                     SelfFlag = 1
                     ReFlag = 1
-                
-            
+
+
             if(Page == 2  and ReFlag == 0):  #photo menu
-                if(GT_Dev.X[0] > 97 and GT_Dev.X[0] < 119 and GT_Dev.Y[0] > 113 and GT_Dev.Y[0] < 136): 
+                if(GT_Dev.X[0] > 97 and GT_Dev.X[0] < 119 and GT_Dev.Y[0] > 113 and GT_Dev.Y[0] < 136):
                     print("Home ...\r\n")
                     Page = 0
                     Read_BMP(PagePath[Page], 0, 0)
                     ReFlag = 1
-                elif(GT_Dev.X[0] > 97 and GT_Dev.X[0] < 119 and GT_Dev.Y[0] > 57 and GT_Dev.Y[0] < 78): 
+                elif(GT_Dev.X[0] > 97 and GT_Dev.X[0] < 119 and GT_Dev.Y[0] > 57 and GT_Dev.Y[0] < 78):
                     print("Next page ...\r\n")
                     Photo_S += 1
                     if(Photo_S > 2): # 6 photos is a maximum of three pages
                         Photo_S=0
                     ReFlag = 2
-                elif(GT_Dev.X[0] > 97 and GT_Dev.X[0] < 119 and GT_Dev.Y[0] > 169 and GT_Dev.Y[0] < 190): 
+                elif(GT_Dev.X[0] > 97 and GT_Dev.X[0] < 119 and GT_Dev.Y[0] > 169 and GT_Dev.Y[0] < 190):
                     print("Last page ...\r\n")
                     if(Photo_S == 0):
                         print("Top page ...\r\n")
                     else:
                         Photo_S -= 1
                         ReFlag = 2
-                elif(GT_Dev.X[0] > 97 and GT_Dev.X[0] < 119 and GT_Dev.Y[0] > 220 and GT_Dev.Y[0] < 242): 
+                elif(GT_Dev.X[0] > 97 and GT_Dev.X[0] < 119 and GT_Dev.Y[0] > 220 and GT_Dev.Y[0] < 242):
                     print("Refresh ...\r\n")
                     SelfFlag = 1
                     ReFlag = 1
@@ -188,45 +188,45 @@ try:
                     ReFlag = 1
                     Read_BMP(PagePath[Page], 0, 0)
                     Show_Photo_Small(image, Photo_S)   # show small photo
-                
-            
+
+
             if(Page == 3  and ReFlag == 0):     #view the photo
-                if(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 117 and GT_Dev.Y[0] > 4 and GT_Dev.Y[0] < 25): 
+                if(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 117 and GT_Dev.Y[0] > 4 and GT_Dev.Y[0] < 25):
                     print("Photo menu ...\r\n")
                     Page = 2
                     Read_BMP(PagePath[Page], 0, 0)
                     Show_Photo_Small(image, Photo_S)
                     ReFlag = 1
-                elif(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 117 and GT_Dev.Y[0] > 57 and GT_Dev.Y[0] < 78): 
+                elif(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 117 and GT_Dev.Y[0] > 57 and GT_Dev.Y[0] < 78):
                     print("Next photo ...\r\n")
                     Photo_L += 1
                     if(Photo_L > 6):
                         Photo_L = 1
                     ReFlag = 2
-                elif(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 117 and GT_Dev.Y[0] > 113 and GT_Dev.Y[0] < 136): 
+                elif(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 117 and GT_Dev.Y[0] > 113 and GT_Dev.Y[0] < 136):
                     print("Home ...\r\n")
                     Page = 0
                     Read_BMP(PagePath[Page], 0, 0)
                     ReFlag = 1
-                elif(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 117 and GT_Dev.Y[0] > 169 and GT_Dev.Y[0] < 190): 
+                elif(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 117 and GT_Dev.Y[0] > 169 and GT_Dev.Y[0] < 190):
                     print("Last page ...\r\n")
                     if(Photo_L == 1):
                         print("Top photo ...\r\n")
-                    else: 
+                    else:
                         Photo_L -= 1
                         ReFlag = 2
-                elif(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 117 and GT_Dev.Y[0] > 220 and GT_Dev.Y[0] < 242): 
+                elif(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 117 and GT_Dev.Y[0] > 220 and GT_Dev.Y[0] < 242):
                     print("Refresh photo ...\r\n")
                     SelfFlag = 1
                     ReFlag = 1
                 if(ReFlag == 2):    # Refresh large photo
                     ReFlag = 1
                     Show_Photo_Large(image, Photo_L)
-                
+
 except IOError as e:
     logging.info(e)
-    
-except KeyboardInterrupt:    
+
+except KeyboardInterrupt:
     logging.info("ctrl + c:")
     flag_t = 0
     epd.sleep()
