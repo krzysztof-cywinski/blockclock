bbb054d7faf7ce38ff84992139db909202e9a7d8
Krzysztof Cywiński <k.m.cywinski@gmail.com>
2024-11-16 23:18:13 +0100

    Re-added Roboto fonts.

--- src/screen.py
+++ src/screen.py
@@ -1,10 +1,13 @@
+import os
+
 from PIL import Image, ImageDraw, ImageFont
-from TP_lib import EPD
+from TP_lib import epd2in13_V4
 
 from util import get_pic_dir, get_font_dir
 
+
 class Screen():
-  def __init__(self, name: str, epd: EPD):
+  def __init__(self, name: str, epd: epd2in13_V4.EPD):
     self.name = name
     self.epd = epd
     self.image = Image.new('1', (epd.width, epd.height), 255)
@@ -16,20 +19,23 @@ class Screen():
   def render(self) -> Image:
     pass
 
+
 class MenuScreen(Screen):
-  def __init__(self, epd: EPD):
-    super.__init__("Menu", epd)
+  def __init__(self, epd: epd2in13_V4.EPD):
+    super(MenuScreen, self).__init__("Menu", epd)
 
   def render(self) -> Image:
     return Image.open(os.path.join(picdir, 'Menu.bmp'))
 
+
 class TextScreen(Screen):
-  def __init__(self, epd: EPD, font: str, font_size: int, initial_text: str = ''):
-    super.__init__("Text", epd)
+  def __init__(self, epd: epd2in13_V4.EPD, font: str, font_size: int, initial_text: str = ''):
+    super(TextScreen, self).__init__("Text", epd)
     self.text = initial_text
-    self.font = ImageFont.truetype(os.path.join(get_font_dir(), font), font_size)
+    self.font = ImageFont.truetype(
+        os.path.join(get_font_dir(), font), font_size)
     self.font_size = font_size
 
   def render(self) -> Image:
-    self.draw.text((2, 2), u'testing ₿', font=self.font, fill=0)
+    self.draw.text((2, 2), 'test', font=self.font, fill=0)
     return self.image
