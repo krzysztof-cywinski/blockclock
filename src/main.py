7805fb576166dd7bf5ac10c11436c401a6e24dff
Krzysztof Cywiński <k.m.cywinski@gmail.com>
2024-11-17 15:37:39 +0100

    Code cleanup. Moved btc price fetching to thread

--- src/main.py
+++ src/main.py
@@ -26,35 +26,30 @@ from screen import TextScreen
 logging.basicConfig(level=logging.DEBUG)
 flag_t = 1
 
-def pthread_irq() :
-    print("pthread running")
+def get_btc_price() -> int:
+    resp = requests.get(
+        url='https://api.coindesk.com/v1/bpi/currentprice.json')
+    data = resp.json()
+    price = data['bpi']['USD']['rate'].replace(',', '')
+    return int(float(price))
+
+btc_price = get_btc_price()
+
+def pthread_irq():
+    print("irq pthread running")
     while flag_t == 1 :
         if(gt.digital_read(gt.INT) == 0) :
             GT_Dev.Touch = 1
         else :
             GT_Dev.Touch = 0
-    print("thread:exit")
+    print("irq thread:exit")
 
-def Show_Photo_Small(image, small):
-    for t in range(1, 5):
-        if(small*2+t > 6):
-            newimage = Image.open(os.path.join(picdir, PhotoPath_S[0]))
-            image.paste(newimage, ((t-1)//2*45+2, (t%2)*124+2))
-        else:
-            newimage = Image.open(os.path.join(picdir, PhotoPath_S[small*2+t]))
-            image.paste(newimage, ((t-1)//2*45+2, (t%2)*124+2))
-
-def Show_Photo_Large(image, large):
-    if(large > 6):
-        newimage = Image.open(os.path.join(picdir, PhotoPath_L[0]))
-        image.paste(newimage, (2, 2))
-    else:
-        newimage = Image.open(os.path.join(picdir, PhotoPath_L[large]))
-        image.paste(newimage, (2, 2))
-
-def Read_BMP(File, x, y):
-    newimage = Image.open(os.path.join(picdir, File))
-    image.paste(newimage, (x, y))
+def pthread_btc_price():
+    print("price pthread running")
+    while flag_t == 1:
+        btc_price = get_btc_price()
+        sleep(5)
+    print("price thread:exit")
 
 try:
     logging.info("epd2in13_V4 Touch Demo")
@@ -70,178 +65,36 @@ try:
     gt.GT_Init()
     epd.Clear(0xFF)
 
-    t = threading.Thread(target = pthread_irq)
-    t.setDaemon(True)
-    t.start()
+    irq_t = threading.Thread(target=pthread_irq)
+    irq_t.daemon = True
+    irq_t.start()
+
+    btc_price_t = threading.Thread(target=pthread_btc_price)
+    btc_price_t.daemon = True
+    btc_price_t.start()
 
     screen = TextScreen(epd, 'paplane-regular.ttf', 42)
 
     initialized = False
     while(1):
-        resp = requests.get(url='https://api.coindesk.com/v1/bpi/currentprice.json')
-        data = resp.json()
-        price = data['bpi']['USD']['rate'].replace(',', '')
-        price = int(float(price))
-        logging.info(price)
-
-        screen.set_text(u'₿/$ '+str(price))
+        screen.set_text(u'₿/$ '+str(btc_price))
         image = screen.render()
         if initialized:
             epd.displayPartial_Wait(epd.getbuffer(image))
         else:
             epd.displayPartBaseImage(epd.getbuffer(image))
             initialized = True
-        # DrawImage = ImageDraw.Draw(image)
-        # epd.init(epd.PART_UPDATE)
+
+        # Read the touch input
+        gt.GT_Scan(GT_Dev, GT_Old)
+        if(GT_Old.X[0] == GT_Dev.X[0] and GT_Old.Y[0] == GT_Dev.Y[0] and GT_Old.S[0] == GT_Dev.S[0]):
+            continue
+
+        if(GT_Dev.TouchpointFlag):
+            GT_Dev.TouchpointFlag = 0
+            logging.debug('Touched X={} Y={}'.format(GT_Dev.X[0], GT_Dev.Y[0]))
+
         time.sleep(5)
-
-
-    # i = j = k = ReFlag = SelfFlag = Page = Photo_L = Photo_S = 0
-    # PhotoPath_S = [ "Photo_1_0.bmp",
-    #                 "Photo_1_1.bmp", "Photo_1_2.bmp", "Photo_1_3.bmp", "Photo_1_4.bmp",
-    #                 "Photo_1_5.bmp", "Photo_1_6.bmp",
-    #                 ]
-    # PhotoPath_L = [ "Photo_2_0.bmp",
-    #                 "Photo_2_1.bmp", "Photo_2_2.bmp", "Photo_2_3.bmp", "Photo_2_4.bmp",
-    #                 "Photo_2_5.bmp", "Photo_2_6.bmp",
-    #                 ]
-    # PagePath = ["Menu.bmp", "White_board.bmp", "Photo_1.bmp", "Photo_2.bmp"]
-
-    # while(1):
-    #     if(i > 12 or ReFlag == 1):
-    #         if(Page == 1 and SelfFlag == 0):
-    #             epd.displayPartial(epd.getbuffer(image))
-    #         else:
-    #             epd.displayPartial_Wait(epd.getbuffer(image))
-    #         i = 0
-    #         k = 0
-    #         j += 1
-    #         ReFlag = 0
-    #         print("*** Draw Refresh ***\r\n")
-    #     elif(k>50000 and i>0 and Page == 1):
-    #         epd.displayPartial(epd.getbuffer(image))
-    #         i = 0
-    #         k = 0
-    #         j += 1
-    #         print("*** Overtime Refresh ***\r\n")
-    #     elif(j > 50 or SelfFlag):
-    #         SelfFlag = 0
-    #         j = 0
-    #         epd.init(epd.FULL_UPDATE)
-    #         epd.displayPartBaseImage(epd.getbuffer(image))
-    #         epd.init(epd.PART_UPDATE)
-    #         print("--- Self Refresh ---\r\n")
-    #     else:
-    #         k += 1
-    #     # Read the touch input
-    #     gt.GT_Scan(GT_Dev, GT_Old)
-    #     if(GT_Old.X[0] == GT_Dev.X[0] and GT_Old.Y[0] == GT_Dev.Y[0] and GT_Old.S[0] == GT_Dev.S[0]):
-    #         continue
-
-    #     if(GT_Dev.TouchpointFlag):
-    #         i += 1
-    #         GT_Dev.TouchpointFlag = 0
-
-    #         if(Page == 0  and ReFlag == 0):     #main menu
-    #             if(GT_Dev.X[0] > 29 and GT_Dev.X[0] < 92 and GT_Dev.Y[0] > 56 and GT_Dev.Y[0] < 95):
-    #                 print("Photo ...\r\n")
-    #                 Page = 2
-    #                 Read_BMP(PagePath[Page], 0, 0)
-    #                 Show_Photo_Small(image, Photo_S)
-    #                 ReFlag = 1
-    #             elif(GT_Dev.X[0] > 29 and GT_Dev.X[0] < 92 and GT_Dev.Y[0] > 153 and GT_Dev.Y[0] < 193):
-    #                 print("Draw ...\r\n")
-    #                 Page = 1
-    #                 Read_BMP(PagePath[Page], 0, 0)
-    #                 ReFlag = 1
-
-
-    #         if(Page == 1 and ReFlag == 0):   #white board
-    #             DrawImage.rectangle([(GT_Dev.X[0], GT_Dev.Y[0]), (GT_Dev.X[0] + GT_Dev.S[0]/8 + 1, GT_Dev.Y[0] + GT_Dev.S[0]/8 + 1)], fill=0)
-    #             if(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 118 and GT_Dev.Y[0] > 6 and GT_Dev.Y[0] < 30):
-    #                 print("Home ...\r\n")
-    #                 Page = 1
-    #                 Read_BMP(PagePath[Page], 0, 0)
-    #                 ReFlag = 1
-    #             elif(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 118 and GT_Dev.Y[0] > 113 and GT_Dev.Y[0] < 136):
-    #                 print("Clear ...\r\n")
-    #                 Page = 0
-    #                 Read_BMP(PagePath[Page], 0, 0)
-    #                 ReFlag = 1
-    #             elif(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 118 and GT_Dev.Y[0] > 220 and GT_Dev.Y[0] < 242):
-    #                 print("Refresh ...\r\n")
-    #                 SelfFlag = 1
-    #                 ReFlag = 1
-
-
-    #         if(Page == 2  and ReFlag == 0):  #photo menu
-    #             if(GT_Dev.X[0] > 97 and GT_Dev.X[0] < 119 and GT_Dev.Y[0] > 113 and GT_Dev.Y[0] < 136):
-    #                 print("Home ...\r\n")
-    #                 Page = 0
-    #                 Read_BMP(PagePath[Page], 0, 0)
-    #                 ReFlag = 1
-    #             elif(GT_Dev.X[0] > 97 and GT_Dev.X[0] < 119 and GT_Dev.Y[0] > 57 and GT_Dev.Y[0] < 78):
-    #                 print("Next page ...\r\n")
-    #                 Photo_S += 1
-    #                 if(Photo_S > 2): # 6 photos is a maximum of three pages
-    #                     Photo_S=0
-    #                 ReFlag = 2
-    #             elif(GT_Dev.X[0] > 97 and GT_Dev.X[0] < 119 and GT_Dev.Y[0] > 169 and GT_Dev.Y[0] < 190):
-    #                 print("Last page ...\r\n")
-    #                 if(Photo_S == 0):
-    #                     print("Top page ...\r\n")
-    #                 else:
-    #                     Photo_S -= 1
-    #                     ReFlag = 2
-    #             elif(GT_Dev.X[0] > 97 and GT_Dev.X[0] < 119 and GT_Dev.Y[0] > 220 and GT_Dev.Y[0] < 242):
-    #                 print("Refresh ...\r\n")
-    #                 SelfFlag = 1
-    #                 ReFlag = 1
-    #             elif(GT_Dev.X[0] > 2 and GT_Dev.X[0] < 90 and GT_Dev.Y[0] > 2 and GT_Dev.Y[0] < 248 and ReFlag == 0):
-    #                 print("Select photo ...\r\n")
-    #                 Page = 3
-    #                 Read_BMP(PagePath[Page], 0, 0)
-    #                 Photo_L = int(GT_Dev.X[0]/46*2 + 2-GT_Dev.Y[0]/124 + Photo_S*2)
-    #                 Show_Photo_Large(image, Photo_L)
-    #                 ReFlag = 1
-    #             if(ReFlag == 2):  # Refresh small photo
-    #                 ReFlag = 1
-    #                 Read_BMP(PagePath[Page], 0, 0)
-    #                 Show_Photo_Small(image, Photo_S)   # show small photo
-
-
-    #         if(Page == 3  and ReFlag == 0):     #view the photo
-    #             if(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 117 and GT_Dev.Y[0] > 4 and GT_Dev.Y[0] < 25):
-    #                 print("Photo menu ...\r\n")
-    #                 Page = 2
-    #                 Read_BMP(PagePath[Page], 0, 0)
-    #                 Show_Photo_Small(image, Photo_S)
-    #                 ReFlag = 1
-    #             elif(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 117 and GT_Dev.Y[0] > 57 and GT_Dev.Y[0] < 78):
-    #                 print("Next photo ...\r\n")
-    #                 Photo_L += 1
-    #                 if(Photo_L > 6):
-    #                     Photo_L = 1
-    #                 ReFlag = 2
-    #             elif(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 117 and GT_Dev.Y[0] > 113 and GT_Dev.Y[0] < 136):
-    #                 print("Home ...\r\n")
-    #                 Page = 0
-    #                 Read_BMP(PagePath[Page], 0, 0)
-    #                 ReFlag = 1
-    #             elif(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 117 and GT_Dev.Y[0] > 169 and GT_Dev.Y[0] < 190):
-    #                 print("Last page ...\r\n")
-    #                 if(Photo_L == 1):
-    #                     print("Top photo ...\r\n")
-    #                 else:
-    #                     Photo_L -= 1
-    #                     ReFlag = 2
-    #             elif(GT_Dev.X[0] > 96 and GT_Dev.X[0] < 117 and GT_Dev.Y[0] > 220 and GT_Dev.Y[0] < 242):
-    #                 print("Refresh photo ...\r\n")
-    #                 SelfFlag = 1
-    #                 ReFlag = 1
-    #             if(ReFlag == 2):    # Refresh large photo
-    #                 ReFlag = 1
-    #                 Show_Photo_Large(image, Photo_L)
 
 except IOError as e:
     logging.info(e)
@@ -251,6 +104,7 @@ except KeyboardInterrupt:
     flag_t = 0
     epd.sleep()
     time.sleep(2)
-    t.join()
+    irq_t.join()
+    btc_price_t.join()
     epd.Dev_exit()
     exit()
